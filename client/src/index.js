53cb3b2d11e151cfefd18afaf0b87519bece3fba
vovanky99 <150527055+vovanky99@users.noreply.github.com>
2023-11-29 20:42:51 +0700

    required library in reactjs

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tippy.js/dist/tippy.css';
+import { Button } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
